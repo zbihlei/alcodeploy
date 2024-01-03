a3b1f74eb28758102ea7072633e6812fd1b27828
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-03 20:21:47 +0200

    cors appuse + appoptions

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typeDefs.js';
+import resolvers from './resolvers.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const name = process.env.NAME;
+const password = process.env.PASS;
+
 async function startServer() {
   const app = express();
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
   });
+  
 
   await apolloServer.start();
 
@@ -18,7 +29,8 @@ async function startServer() {
     credentials: true
   }));
 
-  apolloServer.applyMiddleware({ app });
+
+  apolloServer.applyMiddleware({ app: app });
 
   app.use(express.json());
 
@@ -32,6 +44,7 @@ async function startServer() {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
+  
 
   app.listen(4000, () => console.log('server is running on port 4000'));
 }
