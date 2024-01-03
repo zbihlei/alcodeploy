a1b6daa33da014f7898e2daa36d4a69c82d200a7
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-03 20:17:18 +0200

    server change1

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,24 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import typeDefs from './typeDefs.js';
-import resolvers from './resolvers.js';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-const name = process.env.NAME;
-const password = process.env.PASS;
-
 async function startServer() {
   const app = express();
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-    cors: {
-      origin: "https://alcodeploy-frontend.vercel.app",
-      credentials: true
-    }
   });
-  
 
   await apolloServer.start();
 
-  apolloServer.applyMiddleware({ app: app });
+  app.use(cors({
+    origin: "https://alcodeploy-frontend.vercel.app",
+    credentials: true
+  }));
+
+  app.options('*', cors({
+    origin: "https://alcodeploy-frontend.vercel.app",
+    methods: ["POST", "GET"],
+    credentials: true
+  }));
+
+  apolloServer.applyMiddleware({ app });
 
   app.use(express.json());
 
@@ -36,7 +32,6 @@ async function startServer() {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-  
 
   app.listen(4000, () => console.log('server is running on port 4000'));
 }
