d34e0dba65e3c3d5707bcf06ec22400f283f57ef
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-04 16:05:56 +0200

    apolloclient uri

--- frontend/apolloClient.js
+++ frontend/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', 
+  uri: 'https://alcodeploy-frontend.vercel.app/api/graphql' , 
   cache: new InMemoryCache(),
 });
 
