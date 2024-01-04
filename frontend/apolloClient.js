3fff19adc0abc7074b5ad01b123f3ee363f14b38
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-04 16:42:03 +0200

    apollo client change

--- frontend/apolloClient.js
+++ frontend/apolloClient.js
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloLink } from 'apollo-link';
+import { logger } from 'apollo-link-logger';
+
+const httpLink = createHttpLink({
+  uri: 'https://alcodeploy-api.vercel.app/api/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'https://alcodeploy-api.vercel.app/api/graphql' , 
+  link: ApolloLink.from([logger, authLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
