aef16d9a88a59d9ae8d803c33bfabfb71051668c
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-04 17:11:16 +0200

    constat localhost changed

--- frontend/app/utils/constants.js
+++ frontend/app/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8800";
-export const LOCAL_HOST = "https://alcodeploy-api.vercel.app";
+export const LOCAL_HOST = "https://alcodeploy-frontend.vercel.app";
