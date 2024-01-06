5be6f025d11727e98419771e4f55bff2293efaa4
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-06 16:19:28 +0200

    change spinner

--- frontend/app/components/Spinner.jsx
+++ frontend/app/components/Spinner.jsx
@@ -1,7 +1,7 @@
 
 const Spinner = () => {
     return (
-      <svg style={{width: '5%', overflow: 'visible'}} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='none' d='M0 0h200v200H0z'></path><path fill='none' stroke-linecap='round' stroke='#F21EA8' stroke-width='24' transform-origin='center' d='M70 95.5V112m0-84v16.5m0 0a25.5 25.5 0 1 0 0 51 25.5 25.5 0 0 0 0-51Zm36.4 4.5L92 57.3M33.6 91 48 82.7m0-25.5L33.6 49m58.5 33.8 14.3 8.2'><animateTransform type='rotate' attributeName='transform' calcMode='spline' dur='1' values='0;-120' keyTimes='0;1' keySplines='0 0 1 1' repeatCount='indefinite'></animateTransform></path><path fill='none' stroke-linecap='round' stroke='#F21EA8' stroke-width='24' transform-origin='center' d='M130 155.5V172m0-84v16.5m0 0a25.5 25.5 0 1 0 0 51 25.5 25.5 0 0 0 0-51Zm36.4 4.5-14.3 8.3M93.6 151l14.3-8.3m0-25.4L93.6 109m58.5 33.8 14.3 8.2'><animateTransform type='rotate' attributeName='transform' calcMode='spline' dur='1' values='0;120' keyTimes='0;1' keySplines='0 0 1 1' repeatCount='indefinite'></animateTransform></path></svg>
+<svg style={{width: '5%', overflow: 'visible'}} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 200 200"><path fill="#F21EA8" stroke="#F21EA8" stroke-width="21" transform-origin="center" d="m148 84.7 13.8-8-10-17.3-13.8 8a50 50 0 0 0-27.4-15.9v-16h-20v16A50 50 0 0 0 63 67.4l-13.8-8-10 17.3 13.8 8a50 50 0 0 0 0 31.7l-13.8 8 10 17.3 13.8-8a50 50 0 0 0 27.5 15.9v16h20v-16a50 50 0 0 0 27.4-15.9l13.8 8 10-17.3-13.8-8a50 50 0 0 0 0-31.7Zm-47.5 50.8a35 35 0 1 1 0-70 35 35 0 0 1 0 70Z"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="1" values="0;120" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></path></svg>  
     )
   }
   
