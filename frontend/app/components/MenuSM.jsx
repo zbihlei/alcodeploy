dbd6bfe6f6577a3d0ab88ba692464397b7efb28b
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-06 13:04:28 +0200

    ooption vis visible

--- frontend/app/components/MenuSM.jsx
+++ frontend/app/components/MenuSM.jsx
@@ -4,29 +4,31 @@ import styles from '../styles/menusm.module.scss';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import {useAuth} from '../hooks/useAuth';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const MenuSM = ({onClick}) => {
 
     const {isAuth, email} = useAuth();
     const basket= useSelector((state)=>state.basket.basket);
-    // const [clicked, setClicked] = useState(true);
+    const [visible, setVisible] = useState(false);
 
-    // useEffect(()=>{
-    //     setClicked((prevClicked) => !prevClicked);   
-    // },[onClick])
+    useEffect(() => {
+      if (onClick && typeof onClick === 'function') {
+        setVisible(true);
+      }
+    }, [onClick]);
 
   return (
     
 <div
   className={styles.right_sm}
   style={{
-    visibility: clicked ? 'visible' : 'hidden',
-    opacity: clicked ? '1' : '0',
+    visibility: visible ? 'visible' : 'hidden',
+    opacity: visible ? '1' : '0',
     transition: 'visibility 0.3s, opacity 0.3s ease-in-out'
   }}
 > 
-  <button className={styles.close} onClick={onClick}>X</button>  
+  <button className={styles.close} onClick={() => setVisible((prevVisible) => !prevVisible)}>X</button>  
    {isAuth ? 
     <>
       <div className={styles.name}> <span>Hello</span>  <Link href='/user' className={styles.link}>{email}</Link> </div>
